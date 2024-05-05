234de6d9d076a48321b3255ec7d4a0abc2c2515a
thomasbravin <146086155+thomasbravin@users.noreply.github.com>
2024-05-06 00:34:30 +0200

    Add files via upload

---
+++ TempView/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace TempView
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // cartesianChart1
+            // 
+            cartesianChart1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            cartesianChart1.Location = new Point(12, 12);
+            cartesianChart1.Name = "cartesianChart1";
+            cartesianChart1.Size = new Size(864, 621);
+            cartesianChart1.TabIndex = 2;
+            cartesianChart1.Text = "cartesianChart1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(888, 645);
+            Controls.Add(cartesianChart1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+		private LiveCharts.WinForms.CartesianChart cartesianChart1;
+	}
+}
---
+++ TempView/Form1.cs
@@ -0,0 +1,99 @@
+using LiveCharts.Wpf;
+using LiveCharts;
+using System.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Windows.Media;
+
+namespace TempView
+{
+	public partial class Form1 : Form
+	{
+		public static int samples = 20;
+		public class Dato
+		{
+			public int Id { get; set; }
+			public DateTime DateTime { get; set; }
+			public string Temperatura { get; set; }
+			public double NumTemp { get; set; }
+		}
+
+		public static List<Dato> dati;
+
+		public Form1()
+		{
+			InitializeComponent();
+			timer1.Enabled = true;
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double>()
+                }
+            };
+			for (int i = 0; i < samples; i++)
+			{
+				cartesianChart1.Series[0].Values.Add(0.00);
+            }
+        }
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			readDB();
+			List<double> temperature = new List<double>();
+			foreach (var dato in dati)
+			{
+				temperature.Add(Convert.ToDouble(dato.Temperatura.Replace('.', ',')));
+			}
+						
+			for (int i = 0; i < samples; i++)
+			{
+				cartesianChart1.Series[0].Values[i] = temperature[i];
+			}
+						
+
+		}
+
+
+		public static void readDB()
+		{
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;" +
+            "AttachDbFilename=C:\\Users\\Thomas Bravin\\Desktop\\2iot-progettostage-main\\2IOTDB.mdf;" +
+            "Integrated Security=True;" +
+            "Connect Timeout=30;";
+
+            dati = new List<Dato>();
+			
+			try
+			{
+				using (SqlConnection conn = new SqlConnection(connectionString))
+				{
+					conn.Open();
+					string query = $"SELECT TOP {samples} * FROM Dati ORDER BY Id DESC";
+
+					using (SqlCommand cmd = new SqlCommand(query, conn))
+					{
+						using (SqlDataReader reader = cmd.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								Dato dato = new Dato();
+								dato.Id = reader.GetInt32(0);
+								dato.DateTime = reader.GetDateTime(1);
+								dato.Temperatura = reader.GetString(2);
+								dati.Add(dato);
+							}
+						}
+					}
+					dati.Reverse();
+					conn.Close();
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+					
+		}
+	}
+}
